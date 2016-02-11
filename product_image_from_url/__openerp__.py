6cc9e056a08e1dc3a05350d71181780750d6c616
Cyrus Waithaka <cygawa@gmail.com>
2016-02-12 02:16:24 +0300

    add product_image_from_url module

---
+++ product_image_from_url/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Import Product Images From URL",
+    'summary': """
+        Get product images from url""",
+    'description': """
+        This module offers a way to import product images from urls.
+		Simply import the url of the product image in the Image Url field.
+        Then select the images and download images. This supports batch processing.
+    """,
+    'author': "Explore Data Systems",
+    'website': "http://www.exploredatasystems.com",
+    'category': 'Specific Industry Applications',
+    #Change the version every release for apps.
+    'version': '0.1',
+	#the cost of the module in EUR
+	'price' : '20',
+    # any module necessary for this one to work correctly
+    'depends': [
+        'product',
+    ],
+    # always loaded
+    'data': [
+        'views/product_view.xml',	
+		'views/product_actions.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+    # only loaded in test
+    'test': [
+    ],
+}
---
+++ product_image_from_url/models/product.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+import base64#file encode
+import urllib2 #file download from url
+
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class product_template(osv.osv):
+    _inherit = "product.template"
+    _columns = {
+        'web':fields.char('Image url', help='Automatically sanitized HTML contents'),
+        }
+    def onchange_image(self,cr,uid,ids,web,context=None):
+        link=web
+        photo = base64.encodestring(urllib2.urlopen(link).read())
+        val={
+             'image_medium':photo,
+             }
+        return {'value': val}
+
+    def download_product_image(self,cr,uid,ids,context=None):
+        for product in self.browse(cr, uid, ids, context=context):
+            link = product.web
+            if link and link!='':
+                try:
+                    photo = base64.encodestring(urllib2.urlopen(link, timeout=10).read())
+                    val={
+                         'image_medium':photo,
+                         }
+                    product.write(val)
+                except urllib2.HTTPError, e:
+                    _logger.error('HTTPError = ' + str(e.code)+' when processing product id: '+str(product.id))
+                    continue
+                except urllib2.URLError, e:
+                    _logger.error('URLError = ' + str(e.reason)+' when processing product id: '+str(product.id))
+                    continue
+                except httplib.HTTPException, e:
+                    _logger.error('HTTPException'+' when processing product id: '+str(product.id))
+                    continue
+                except Exception:
+                    continue
+                    import traceback
+                    _logger.error('generic exception: ' + traceback.format_exc()+' when processing product id: '+str(product.id))
+        return True
+
+product_template()
