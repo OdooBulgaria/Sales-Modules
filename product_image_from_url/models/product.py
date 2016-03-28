89dbf4b49223621f2ce1093c3d87ca32bddea725
Cyrus Waithaka <cygawa@gmail.com>
2016-03-29 00:58:16 +0300

    resolve error on empty url. Add error catch

--- product_image_from_url/models/product.py
+++ product_image_from_url/models/product.py
@@ -19,11 +19,23 @@ class product_template(osv.osv):
         }
     def onchange_image(self,cr,uid,ids,web,context=None):
         link=web
-        photo = base64.encodestring(urllib2.urlopen(link).read())
-        val={
-             'image_medium':photo,
-             }
-        return {'value': val}
+        if link and link!='':
+            try:
+                photo = base64.encodestring(urllib2.urlopen(link, timeout=10).read())
+                val={
+                     'image_medium':photo,
+                     }
+                return {'value': val}
+            except urllib2.HTTPError, e:
+                _logger.error('HTTPError = ' + str(e.code)+' when processing product id: '+str(product.id))
+            except urllib2.URLError, e:
+                _logger.error('URLError = ' + str(e.reason)+' when processing product id: '+str(product.id))
+            except httplib.HTTPException, e:
+                _logger.error('HTTPException'+' when processing product id: '+str(product.id))
+            except Exception:
+                import traceback
+                _logger.error('generic exception: ' + traceback.format_exc()+' when processing product id: '+str(product.id))
+        return True
 
     def download_product_image(self,cr,uid,ids,context=None):
         for product in self.browse(cr, uid, ids, context=context):
@@ -45,9 +57,9 @@ class product_template(osv.osv):
                     _logger.error('HTTPException'+' when processing product id: '+str(product.id))
                     continue
                 except Exception:
-                    continue
                     import traceback
                     _logger.error('generic exception: ' + traceback.format_exc()+' when processing product id: '+str(product.id))
+                    continue
         return True
 
 product_template()
