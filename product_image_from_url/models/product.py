843c5ddca541cb3020620d80d94c44dca101e0cd
Cyrus Waithaka <cygawa@gmail.com>
2016-03-29 01:05:07 +0300

    resolve error on empty url. Add error catch

--- product_image_from_url/models/product.py
+++ product_image_from_url/models/product.py
@@ -27,14 +27,14 @@ class product_template(osv.osv):
                      }
                 return {'value': val}
             except urllib2.HTTPError, e:
-                _logger.error('HTTPError = ' + str(e.code)+' when processing product id: '+str(product.id))
+                _logger.error('HTTPError = ' + str(e.code)+' when processing this request')
             except urllib2.URLError, e:
-                _logger.error('URLError = ' + str(e.reason)+' when processing product id: '+str(product.id))
+                _logger.error('URLError = ' + str(e.reason)+' when processing this request')
             except httplib.HTTPException, e:
-                _logger.error('HTTPException'+' when processing product id: '+str(product.id))
+                _logger.error('HTTPException'+' when processing product id this request')
             except Exception:
                 import traceback
-                _logger.error('generic exception: ' + traceback.format_exc()+' when processing product id: '+str(product.id))
+                _logger.error('generic exception: ' + traceback.format_exc()+' when processing this request')
         return True
 
     def download_product_image(self,cr,uid,ids,context=None):
